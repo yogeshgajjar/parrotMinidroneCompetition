96338c1752ba3eeba13021134dbd9c490c0740ff
Yogesh <yogeshgajjar75@gmail.com>
2020-11-29 21:54:10 -0800

    stl modifications

--- STL.m
+++ STL.m
@@ -11,3 +11,13 @@ init_x_pos = init.posNED(1);
 init_y_pos = init.posNED(2);
 init_z_pos = init.posNED(3);
 
+%%
+
+S = BreachSimulinkSystem('flightControlSystem');
+
+
+%%
+
+STL_ReadFile('specifications.stl');
+fprintf('Checking safe following ..');
+robs1 = S.CheckSpec(safeFollowing);
